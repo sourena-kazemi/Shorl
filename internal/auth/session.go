aace21052017da285963fff71208ecb46aa52b22
sourena-kazemi <kazemisourena@gmail.com>
2025-07-16 15:30:31 +0330

    changed error handling

--- internal/auth/session.go
+++ internal/auth/session.go
@@ -10,25 +10,18 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// type session struct {
-// 	session_id string
-// 	user_id    int
-// 	created_at time.Time
-// 	expires_at time.Time
-// }
-
 func GenerateSession(userID int) (string, error) {
 	sessionID := uuid.NewString()
 	createdAt := time.Now()
 	expiresAt := time.Now().Add(time.Hour * 2)
 	db, err := sql.Open("sqlite3", "./internal/db/app.db")
 	if err != nil {
-		return "", fmt.Errorf("failed to open database connection : %v", err)
+		return "", err
 	}
 	defer db.Close()
 	_, err = db.Exec("INSERT INTO sessions (session_id,user_id,created_at,expires_at) VALUES (?,?,?,?)", sessionID, userID, createdAt, expiresAt)
 	if err != nil {
-		return "", fmt.Errorf("failed to create session : %v", err)
+		return "", err
 	}
 	return sessionID, nil
 }
@@ -39,18 +32,18 @@ func GetUserIdFromSessions(w http.ResponseWriter, sessionID string) (int, error)
 
 	db, err := sql.Open("sqlite3", "./internal/db/app.db")
 	if err != nil {
-		return userID, fmt.Errorf("failed to open database connection : %v", err)
+		return userID, err
 	}
 	defer db.Close()
 
 	err = db.QueryRow("SELECT user_id,expires_at FROM sessions WHERE session_id = ?", sessionID).Scan(&userID, &expiresAt)
 	if err != nil {
-		return userID, fmt.Errorf("failed to fetch user session from database : %v", err)
+		return userID, err
 	}
 	if time.Now().After(expiresAt) {
 		err = DeleteSession(w, sessionID)
 		if err != nil {
-			return userID, fmt.Errorf("failed to remove expired session from database : %v", err)
+			return userID, err
 		}
 		return userID, fmt.Errorf("session has expired")
 	}
@@ -60,13 +53,13 @@ func GetUserIdFromSessions(w http.ResponseWriter, sessionID string) (int, error)
 func DeleteSession(w http.ResponseWriter, sessionID string) error {
 	db, err := sql.Open("sqlite3", "./internal/db/app.db")
 	if err != nil {
-		return fmt.Errorf("failed to open database connection : %v", err)
+		return err
 	}
 	defer db.Close()
 
 	_, err = db.Exec("DELETE FROM sessions WHERE session_id = ?", sessionID)
 	if err != nil {
-		return fmt.Errorf("failed to remove expired session from database : %v", err)
+		return err
 	}
 	http.SetCookie(w, &http.Cookie{
 		Name:     "session_id",
--- internal/auth/user_store.go
+++ internal/auth/user_store.go
@@ -3,7 +3,6 @@ package auth
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 
@@ -22,40 +21,40 @@ func GetUserDataFromGithub(accessToken string) (int, error) {
 	requestURL := "https://api.github.com/user"
 	request, err := http.NewRequest(http.MethodGet, requestURL, nil)
 	if err != nil {
-		return userID, fmt.Errorf("failed to create github validation request : %v", err)
+		return userID, err
 	}
 	request.Header.Set("Authorization", "Bearer "+accessToken)
 	request.Header.Set("Accept", "application/vnd.github5+json")
 	client := &http.Client{}
 	response, err := client.Do(request)
 	if err != nil {
-		return userID, fmt.Errorf("failed to send request to github : %v", err)
+		return userID, err
 	}
 	defer response.Body.Close()
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		return userID, fmt.Errorf("failed to read response body : %v", err)
+		return userID, err
 	}
 
 	var user userData
 	err = json.Unmarshal(body, &user)
 	if err != nil {
-		return userID, fmt.Errorf("failed to parse user data : %v", err)
+		return userID, err
 	}
 	db, err := sql.Open("sqlite3", "./internal/db/app.db")
 	if err != nil {
-		return userID, fmt.Errorf("failed to open database connection : %v", err)
+		return userID, err
 	}
 	defer db.Close()
 	_, err = db.Exec(`INSERT INTO users (github_id,name,avatar_url) VALUES (?,?,?)
 	ON CONFLICT(github_id) DO UPDATE SET name=excluded.name,avatar_url=excluded.avatar_url`,
 		user.ID, user.Name, user.AvatarURL)
 	if err != nil {
-		return userID, fmt.Errorf("failed to create/update user : %v", err)
+		return userID, err
 	}
 	err = db.QueryRow("SELECT id FROM users WHERE github_id = ?", user.ID).Scan(&userID)
 	if err != nil {
-		return userID, fmt.Errorf("failed to fetch user id from database : %v", err)
+		return userID, err
 	}
 	return userID, nil
 }
@@ -63,14 +62,14 @@ func GetUserDataFromGithub(accessToken string) (int, error) {
 func GetUserDataFromDB(userID int) (userData, error) {
 	db, err := sql.Open("sqlite3", "./internal/db/app.db")
 	if err != nil {
-		return userData{}, fmt.Errorf("failed to open database connection : %v", err)
+		return userData{}, err
 	}
 	defer db.Close()
 
 	var user userData
 	err = db.QueryRow("SELECT avatar_url,name FROM users WHERE id = ?", userID).Scan(&user.AvatarURL, &user.Name)
 	if err != nil {
-		return userData{}, fmt.Errorf("failed to fetch user data from database : %v", err)
+		return userData{}, err
 	}
 	user.ID = userID
 	return user, nil
